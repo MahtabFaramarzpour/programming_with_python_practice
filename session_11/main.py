c89e65f60f5608a6209eeab324e6251b0237d26e
Student <mahtab.faramarzpour@srbiau.ac.ir>
2025-10-20 17:23:38 +0330

    upload

--- session_11/main.py
+++ session_11/main.py
@@ -1,10 +1,9 @@
 super_market = []
 
-
 import re
 
 
-def divide(a,b):
+def divide(a, b):
     return a / b
 
 
@@ -17,10 +16,12 @@ def name_validator(name):
     if not re.match(r"^[a-zA-Z\s]{3,30}$", name):
         raise ValueError("Invalid Name !!!")
 
+
 def brand_validator(brand):
     if not re.match(r"^[a-zA-Z\s]{3,30}$", brand):
         raise ValueError("Invalid brand !!!")
 
+
 def quantity_validator(quantity):
-    if not quantity == int and 0 < quantity):
+    if not (quantity == int and 0 < quantity):
         raise ValueError("Invalid quantity !!!")
