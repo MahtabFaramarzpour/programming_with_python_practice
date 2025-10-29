416972055a3750bd3601493cf29632bdac3a0177
Mahtab Faramarzpour <mahtab.faramarzpour@srbiau.ac.ir>
2025-10-30 03:03:07 +0330

    Super_market Kamel shod be hamrah # todo

--- session_11/super_market.py
+++ session_11/super_market.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from my_module_supermarket import *
 from datetime import datetime,date
-
+from tkinter import ttk
 
 from session_11.main import id_validator
 
@@ -18,6 +18,7 @@ def save():
         brand_validator(brand.get())
         price_validator(price.get())
         quantity_validator(quantity.get())
+        table.insert("", END,values=tuple(product.values()))
 
 
         expire_date = datetime.strptime(expiration_date.get(), "%Y-%m-%d").date()
@@ -43,8 +44,11 @@ def save():
         messagebox.showerror("Save Error", f"Error: {e}")
 
 def total_price():
-    if not product_list:
-        messagebox.showerror("No Products", "No Products Available")
+    try:
+        total = calculate_total(product_list)
+        messagebox.showinfo("Total Price", f"Total value of all products: {total}\n {to_word(total)} تومان")
+    except Exception as e:
+        messagebox.showinfo("Error", "No Products Available")
         return
 
     total = 0
@@ -59,7 +63,7 @@ window.config(background = "misty rose")
 # ID
 Label (window, text="Id:").place(x=40, y=40)
 Id = IntVar()
-Entry(window, textvariable=id).place(x=160, y=40)
+Entry(window, textvariable=id, state="readonly").place(x=160, y=40)
 
 # Name
 Label (window, text="Name:").place(x=40, y=80)
@@ -86,11 +90,33 @@ Label(window, text="Expiration date\n YYYY-MM-DD").place(x=40, y=240)
 exp_date = StringVar()
 Entry(window, textvariable=exp_date).place(x=160, y=240)
 
-Button(window, text="Save", command=save).place(x=160, y=280, width=150)
-Button(window, text="Total", command=total_price).place(x=160, y=320, width=150)
+Button(window, text="Save", command=save).place(x=150, y=280, width=140)
+Button(window, text="Total", command=total_price).place(x=150, y=320, width=140)
 
 
 
+table = ttk.Treeview(window, columns=[1, 2, 3, 4, 5, 6], height= 14, show="headings")
+
+table.heading("1", text="ID")
+table.heading("2", text="Name")
+table.heading("3", text="Brand")
+table.heading("4", text="Quantity")
+table.heading("5", text="Price")
+table.heading("6", text="Expiration date")
+
+
+table.column(1, width=40)
+table.column(2, width=100)
+table.column(3, width=100)
+table.column(4, width=60)
+table.column(5, width=60)
+table.column(6, width=100)
+
+
+
+
+
+table.place(x=300, y=40)
 window.mainloop()
 
 
